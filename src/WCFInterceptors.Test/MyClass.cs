12584fdb895f0109d0fedd54582de276e2fb31c9
Arialdo Martini <arialdomartini@gmail.com>
2015-08-31 14:57:36 +0200

    A walking skeleton, with 1 passing test

---
+++ src/WCFInterceptors.Test/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace WCFInterceptors.Test
+{
+    public class MyClassTest
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            new MyClass().ReturnTrue().Should().Be(true);
+        }
+    }
+}
+
---
+++ src/WCFInterceptors/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WCFInterceptors
+{
+    public class MyClass
+    {
+        public object ReturnTrue()
+        {
+            return true;
+        }
+    }
+}
+
